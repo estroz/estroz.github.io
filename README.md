9c569c713bf7fee01eff6c8acc67ad1a6ac74b1d
Eric Stroczynski <ericstroczynski@gmail.com>
2016-04-02 12:18:36 -0700

    Completely rerenovated layout, added content for projects.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # estroz.github.io
 Web Portfolio
+
+Showcasing my projects.
+
+[estroz.github.io](estroz.github.io)
---
+++ js/main.js
@@ -0,0 +1,91 @@
+
+$(document).ready(function(){
+
+//Section background transitions
+var sectionTransition = function() {
+
+  $('.project-section')
+  .mouseenter(function() {
+      $(this)
+        .css(
+        'backgroundColor',
+         '#e9fdff'
+       );
+    })
+    .mouseleave(function() {
+        $(this)
+          .css(
+          'backgroundColor',
+           'white'
+         );
+      });
+};
+
+sectionTransition();
+
+
+//When hovering over a project-image, decrease opacity and show button
+var overlay = function() {
+
+  var $imageWrapper = $('.project-image-wrapper,  .project-image-wrapper-full');
+
+
+  if ($(this).has('button')){
+    $imageWrapper.find('.project-button').hide();
+  }
+
+  $imageWrapper.mouseenter(function() {
+      $(this).find('.project-image, project-image-full').css({
+        'opacity': '0.5',
+        'border-color': '#00385a'
+      });
+      if ($(this).has('button')){
+      $(this).find('.project-button').fadeIn(300);
+    }
+    })
+    .mouseleave(function() {
+      $(this).find('.project-image', 'project-image-full').css({
+        'opacity': '1',
+        'border-color': '#d8d8d8'
+      });
+      if ($(this).has('button')){
+      $(this).find('.project-button').fadeOut(200);
+    }
+    });
+
+};
+
+overlay();
+
+//On button click, replace cover div with projet description and move
+//image to the left column
+
+$('.hidden').hide();
+
+var slideSection = function() {
+
+  $('.project-image-wrapper, .project-image-wrapper-full').click(function() {
+
+    var $parentDiv = $(this).parents('.transition-div');
+    var $childToShow = $parentDiv.find('.hidden');
+    var $childToHide = $childToShow.siblings('.project-section');
+
+    $childToShow.fadeIn(2000, hideActiveSection());
+
+    function hideActiveSection() {
+      $childToShow.removeClass('hidden');
+      $childToHide.fadeOut(500, function() {
+        $(this).addClass('hidden');
+      });
+    }
+
+
+
+  });
+
+
+};
+
+slideSection();
+
+});
